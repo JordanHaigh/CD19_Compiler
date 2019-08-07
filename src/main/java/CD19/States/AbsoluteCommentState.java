ac7fcae91250ba12797f5c39e722dd59bd8f1968
BuildTools <jhaigh1997@gmail.com>
2019-08-07 12:33:11 +1000

    Comment state and transitions added

---
+++ src/main/java/CD19/States/AbsoluteCommentState.java
@@ -0,0 +1,18 @@
+package CD19.States;
+
+/*
+ * Jordan Haigh c3256730 CD19
+ * public class AbsoluteeCommentState.java
+ * Currently have /--. A new line will complete the token. Anything else is consumed.
+ * Character input determines state transitions
+ * */
+public class AbsoluteCommentState implements State {
+
+    @Override
+    public void updateState(StateMachine sm, char c) {
+        if(c == '\n')
+            sm.setCurrentState(new CompletedTokenState());
+
+        //everything else is consumed by the comment line
+    }
+}
--- src/main/java/CD19/States/PossibleCommentState.java
+++ src/main/java/CD19/States/PossibleCommentState.java
@@ -2,13 +2,17 @@ package CD19.States;
 
 /*
  * Jordan Haigh c3256730 CD19
- * public class CommentOrDivideState.java
- * First Character is a /. Need to determine if its a comment or a divide. Character input determines state transitions
+ * public class PossibleCommentState.java
+ * Currently have /-. Need to determine the next char if it is a -. Character input determines state transitions
  * */
 public class PossibleCommentState implements State{
 
     @Override
     public void updateState(StateMachine sm, char c) {
+        if(c == '-')
+            sm.setCurrentState(new AbsoluteCommentState());
+        else
+            sm.setCurrentState(new InvalidState());
 
     }
 }
