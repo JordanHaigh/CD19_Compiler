517d99fb0592dee927ea6012ad7b0c7fbfb7a574
BuildTools <jhaigh1997@gmail.com>
2019-09-24 20:51:19 +1000

    Fixed unit tests for initlist

--- src/test/java/NodeTests/NInitListNodeTests.java
+++ src/test/java/NodeTests/NInitListNodeTests.java
@@ -22,72 +22,92 @@ import static org.mockito.Mockito.when;
 
 public class NInitListNodeTests {
 
-
     //NILIST	<initlist>	::=	<init> <initListTail>
     //	<initListTail>	::=	eps | ,<initlist>
 
-    @Mock
-    NInitNode nInitNode;
-
-    @InjectMocks
-    NInitListNode nInitListNode;
-
-    @BeforeEach
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-    }
-
     @Test
-    public void initlist_epsTest(){
+    public void initlist_oneinit(){
+        SetupMocks.setup();
         List<Token> tokens= new ArrayList<>();
 
         tokens.add(new Token(Token.TIDEN,1,1,"prog"));
-        tokens.add(new Token(Token.TEQEQ,1,1,null));
+        tokens.add(new Token(Token.TEQUL,1,1,null));
+        tokens.add(new Token(Token.TILIT,1,1,null));
+
+
         tokens.add(new Token(Token.TILIT,1,1,null));
 
         Parser parser = new Parser(tokens);
+        NInitListNode nInitListNode = new NInitListNode();
+        TreeNode initlist = nInitListNode.make(parser);
 
-        when(nInitNode.make(parser)).thenReturn(new TreeNode(TreeNode.NINIT));
+        assertEquals(TreeNode.NINIT,initlist.getValue());
+    }
 
+    @Test
+    public void initlist_twoinit(){
+        List<Token> tokens= new ArrayList<>();
+
+        tokens.add(new Token(Token.TIDEN,1,1,"aa"));
+        tokens.add(new Token(Token.TEQUL,1,1,null));
+        tokens.add(new Token(Token.TILIT,1,1,null));
+
+        tokens.add(new Token(Token.TCOMA,1,1,null));
+
+        tokens.add(new Token(Token.TIDEN,1,1,"bb"));
+        tokens.add(new Token(Token.TEQUL,1,1,null));
+        tokens.add(new Token(Token.TILIT,1,1,null));
+
+        tokens.add(new Token(Token.TILIT,1,1,null));
+
+
+
+        Parser parser = new Parser(tokens);
+        NInitNode nInitNode = new NInitNode();
+        nInitNode.setnExprNode(NExprNode.INSTANCE());
+        NInitListNode nInitListNode = new NInitListNode(nInitNode);
         TreeNode initlist = nInitListNode.make(parser);
 
         assertEquals(TreeNode.NILIST,initlist.getValue());
         assertEquals(TreeNode.NINIT,initlist.getLeft().getValue());
-        assertEquals(null ,initlist.getRight());
+        assertEquals(TreeNode.NINIT,initlist.getRight().getValue());
     }
 
     @Test
-    public void initList_notEpsTest(){
+    public void initList_threeinits(){
 
-        // Create partial mock
-        NExprNode mockExprNode = mock(NExprNode.class);
-        NInitNode nInitNode = new NInitNode(mockExprNode);
-        NInitListNode nInitListNode = new NInitListNode(nInitNode);
-
+        SetupMocks.setup();
         List<Token> tokens= new ArrayList<>();
 
-        // x = 1, y = 2
-        tokens.add(new Token(Token.TIDEN,1,1,"x"));
-        tokens.add(new Token(Token.TEQUL,1,1, null));
-        tokens.add(new Token(Token.TILIT,1,1, "1"));
+        tokens.add(new Token(Token.TIDEN,1,1,"aa"));
+        tokens.add(new Token(Token.TEQUL,1,1,null));
+        tokens.add(new Token(Token.TILIT,1,1,null));
+
         tokens.add(new Token(Token.TCOMA,1,1,null));
-        tokens.add(new Token(Token.TIDEN,1,1,"y"));
-        tokens.add(new Token(Token.TEQUL,1,1, null));
-        tokens.add(new Token(Token.TILIT,1,1, "2"));
+
+        tokens.add(new Token(Token.TIDEN,1,1,"bb"));
+        tokens.add(new Token(Token.TEQUL,1,1,null));
+        tokens.add(new Token(Token.TILIT,1,1,null));
+
+        tokens.add(new Token(Token.TCOMA,1,1,null));
+
+        tokens.add(new Token(Token.TIDEN,1,1,"cc"));
+        tokens.add(new Token(Token.TEQUL,1,1,null));
+        tokens.add(new Token(Token.TILIT,1,1,null));
+
         tokens.add(new Token(Token.TILIT,1,1,null));
 
         Parser parser = new Parser(tokens);
-
-        when(mockExprNode.make(parser)).thenAnswer((Answer) invocationOnMock -> {
-            parser.consume();
-            return new TreeNode(TreeNode.NILIT);
-        });
-
+        NInitNode nInitNode = new NInitNode();
+        nInitNode.setnExprNode(NExprNode.INSTANCE());
+        NInitListNode nInitListNode = new NInitListNode(nInitNode);
         TreeNode initlist = nInitListNode.make(parser);
 
         assertEquals(TreeNode.NILIST,initlist.getValue());
         assertEquals(TreeNode.NINIT,initlist.getLeft().getValue());
         assertEquals(TreeNode.NILIST ,initlist.getRight().getValue());
+        assertEquals(TreeNode.NINIT ,initlist.getRight().getLeft().getValue());
+        assertEquals(TreeNode.NINIT ,initlist.getRight().getRight().getValue());
     }
 }
 
