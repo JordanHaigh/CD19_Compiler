2cd7b451f26a8a7493b446a6820fad13ca1cc695
BuildTools <jhaigh1997@gmail.com>
2019-10-16 11:33:49 +1100

    Bug fix for function argument with array

--- src/main/java/CD19/Parser/Nodes/NExponentNode.java
+++ src/main/java/CD19/Parser/Nodes/NExponentNode.java
@@ -175,6 +175,15 @@ public class NExponentNode implements Node {
                         String expected = expectedFunctionArgs.get(i);
                         String actual = actualFunctionArgs.get(i);
 
+                        if(expected.contains("Array")){
+                            expected = expected.replaceAll("Array","");
+                        }
+                        if(actual.contains("IsArrayOf")){
+                            actual = actual.replaceAll("IsArrayOf", " ");
+                            String[] split = actual.split(" ");
+                            actual = split[0];
+                        }
+
                         if (!expected.equals(actual)) {
                             parser.semanticError("Argument Types do not match. Expected: " + expected + ", Actual: " + actual, peek);
                         }
